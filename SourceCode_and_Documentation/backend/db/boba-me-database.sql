d560da449de50abdf53fe2bb3808ff60372a92cf
Navid Bhuiyan <navidbhuiyan@Navids-MacBook-Pro.local>
2021-04-17 16:16:09 +1000

    added database data

---
+++ SourceCode_and_Documentation/backend/db/boba-me-database.sql
@@ -0,0 +1,91 @@
+--
+-- PostgreSQL Boba-Me Database (Simple)
+--
+
+CREATE TABLE shop (
+    id integer NOT NULL,
+    name character varying(80) NOT NULL,
+    town character varying(80) NOT NULL,
+    city character varying(80) NOT NULL,
+    country character varying(80) NOT NULL
+);
+
+CREATE TABLE menu (
+    shop_id integer NOT NULL,
+    drink_id integer NOT NULL,
+    price decimal NOT NULL
+);
+
+CREATE TABLE topping (
+    shop_id integer NOT NULL,
+    topping character varying(80) NOT NULL,
+    price decimal NOT NULL
+);
+
+CREATE TABLE shop_opening_times (
+    shop_id integer NOT NULL,
+    day_of_week integer NOT NULL,
+    opening_time character varying(8) NOT NULL,
+    closing_time character varying(8) NOT NULL
+);
+
+CREATE TABLE drink (
+    id integer NOT NULL,
+    name character varying(80) NOT NULL,
+    rating integer
+);
+
+
+-- Insert Dummy Shop Data (0 for Gong and 1 for Chatime)
+INSERT INTO shop VALUES (0, 'Gong Cha', 'Randwick', 'Sydney', 'Australia');
+INSERT INTO shop VALUES (1, 'Chatime', 'Randwick', 'Sydney', 'Australia');
+
+-- Insert Dummy Drink Data
+INSERT INTO drink VALUES (0, 'Milk Foam Green Tea');
+INSERT INTO drink VALUES (1, 'Pearl Milk Tea');
+INSERT INTO drink VALUES (2, 'Royal Milk Tea');
+INSERT INTO drink VALUES (3, 'Earl Grey Milk Tea');
+INSERT INTO drink VALUES (4, 'Milk Foam Green Tea');
+
+-- Insert Dummy Menu Data
+INSERT INTO menu VALUES (0, 0, 6.20);
+INSERT INTO menu VALUES (0, 1, 6.20);
+INSERT INTO menu VALUES (0, 2, 5.50);
+INSERT INTO menu VALUES (0, 3, 5.80);
+INSERT INTO menu VALUES (0, 4, 6.20);
+
+INSERT INTO menu VALUES (1, 0, 6.20);
+INSERT INTO menu VALUES (1, 1, 6.20);
+INSERT INTO menu VALUES (1, 2, 5.50);
+INSERT INTO menu VALUES (1, 3, 5.80);
+INSERT INTO menu VALUES (1, 4, 6.20);
+
+-- Insert Dummy Topping Menu
+INSERT INTO topping VALUES (0, 'Mango Pearls', 0.70);
+INSERT INTO topping VALUES (0, 'Pearls', 0.70);
+INSERT INTO topping VALUES (0, 'Lychee Pearls', 0.70);
+INSERT INTO topping VALUES (0, 'Aloe Vera', 0.70);
+INSERT INTO topping VALUES (0, 'White Pearls', 0.70);
+
+INSERT INTO topping VALUES (1, 'Mango Pearls', 0.70);
+INSERT INTO topping VALUES (1, 'Pearls', 0.70);
+INSERT INTO topping VALUES (1, 'Lychee Pearls', 0.70);
+INSERT INTO topping VALUES (1, 'Aloe Vera', 0.70);
+INSERT INTO topping VALUES (1, 'White Pearls', 0.70);
+
+-- Insert Dummy Shop Opening Time Data
+INSERT INTO shop_opening_times VALUES (0, 0, '9:00 AM', '10:00 PM');
+INSERT INTO shop_opening_times VALUES (0, 1, '9:00 AM', '10:00 PM');
+INSERT INTO shop_opening_times VALUES (0, 2, '9:00 AM', '10:00 PM');
+INSERT INTO shop_opening_times VALUES (0, 3, '9:00 AM', '10:00 PM');
+INSERT INTO shop_opening_times VALUES (0, 4, '9:00 AM', '10:00 PM');
+INSERT INTO shop_opening_times VALUES (0, 5, '9:00 AM', '10:00 PM');
+INSERT INTO shop_opening_times VALUES (0, 6, '9:00 AM', '10:00 PM');
+
+INSERT INTO shop_opening_times VALUES (1, 0, '9:00 AM', '10:00 PM');
+INSERT INTO shop_opening_times VALUES (1, 1, '9:00 AM', '10:00 PM');
+INSERT INTO shop_opening_times VALUES (1, 2, '9:00 AM', '10:00 PM');
+INSERT INTO shop_opening_times VALUES (1, 3, '9:00 AM', '10:00 PM');
+INSERT INTO shop_opening_times VALUES (1, 4, '9:00 AM', '10:00 PM');
+INSERT INTO shop_opening_times VALUES (1, 5, '9:00 AM', '10:00 PM');
+INSERT INTO shop_opening_times VALUES (1, 6, '9:00 AM', '10:00 PM');
