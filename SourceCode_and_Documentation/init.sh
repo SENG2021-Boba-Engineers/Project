fb72838f5ebf893479e7cf7a47da62b204ca630c
Navid Bhuiyan <navidbhuiyan@Navids-MacBook-Pro.local>
2021-04-20 13:18:53 +1000

    updated init.sh

--- SourceCode_and_Documentation/init.sh
+++ SourceCode_and_Documentation/init.sh
@@ -7,8 +7,8 @@
 if !(test -d backend/venv)
 then
     cd backend
-    pip3 install venv 2> /dev/null
-    python3 -m venv venv
+    pip3 install virtualenv 2> /dev/null
+    virtualenv venv
     source venv/bin/activate
     pip install -r requirements.txt 2> /dev/null
     deactivate
